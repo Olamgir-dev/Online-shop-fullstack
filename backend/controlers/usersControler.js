2e5db3274a6876303af6460db76223d8229f0cae
Olamgir <olamgiribrohimov2003@gmail.com>
2023-03-11 12:09:20 +0500

    package.json uzgardi

--- backend/controlers/usersControler.js
+++ backend/controlers/usersControler.js
@@ -2,30 +2,13 @@
 const User = require("../models/usersModel");
 
 const getAllUsers = (req, res) => {
-    User.find({}, (err, data) => {
-        const encryptCaesar = (password) => {
-            const shiferKalit = process.env.SHIFIR_KALIT;
-
-            // Matnni ketma-ketlikka olish
-            const bytes = new TextEncoder().encode(password);
-
-            // Matnni shifrlash
-            let encryptedBytes = new Uint8Array(bytes.length);
-            for (let i = 0; i < bytes.length; i++) {
-                encryptedBytes[i] = (bytes[i] + shiferKalit) % 256;
-            }
-            // Shifrlangan matnning string sifatida qaytarilishi
-            return new TextDecoder().decode(encryptedBytes);
-        }
-        const shifrData = data.map(user => {
-            const password = encryptCaesar(user.password)
-            user.password = password
-            return user
+    try {
+        User.find({}, (err, data) => {
+            res.status(200).json(data)
         })
-  
-        res.status(200).json(data)
-        // res.status(500).json({error: err.message})
-    })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 }
 
 const signup = async (req, res) => {
